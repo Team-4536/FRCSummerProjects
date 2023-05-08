f3890fb59131810ef7ed9ec09512cb7e2c4b1eb6
Emmett <unconfigured@null.spigotmc.org>
2023-05-08 15:45:31 -0500

    Swerve controller most definitely does not work

--- Robot/swerveController.py
+++ Robot/swerveController.py
@@ -1,6 +1,10 @@
 from node import *
+import wpilib
+import math
 import utils.tags as tags
 from hardware.Input import FlymerInputProfile
+from utils.V2 import V2
+from typing import Any
 
 
 class SwerveProf(Node):
@@ -14,18 +18,67 @@ class SwerveProf(Node):
         input = data.get(tags.INPUT)
         if type(input) is not FlymerInputProfile: return
 
+        #grab controller inputs
+        input = data[tags.INPUT]
+        assert type(input) == FlymerInputProfile
 
-        x = input.drive[0]
-        y = input.drive[1]
-        t = input.turning
+        #raw inputs
+        inputX = input.drive[0]
+        inputY = input.drive[1]
+        inputZ = input.turning
 
-        drivePrefs = [ tags.FLDrive, tags.FRDrive, tags.BLDrive, tags.BRDrive ]
-        steerPrefs = [ tags.FLSteering, tags.FRSteering, tags.BLSteering, tags.BRSteering ]
-        for i in range(0, 4):
-            data[drivePrefs[i] + tags.MOTOR_SPEED_CONTROL] = y
-            data[steerPrefs[i] + tags.MOTOR_SPEED_CONTROL] = x
+        inputGyro = 0 #constant for testing
 
-        data[tags.FLSteering + tags.MOTOR_SPEED_CONTROL] = t
+        #assign inputs to vectors
+        leftStick = V2(inputX, inputY)
+        FLTurningVector = V2(math.cos(45) * inputZ, math.cos(45) * inputZ)
+        FRTurningVector = V2(math.cos(45) * inputZ, -math.cos(45) * inputZ)
+        BLTurningVector = V2(-math.cos(45) * inputZ, -math.cos(45) * inputZ)
+        BRTurningVector = V2(-math.cos(45) * inputZ, math.cos(45) * inputZ)
+
+        #field oriented offset (change later from gyro to encoder readings)
+        #leftStick = leftStick.rotateDegrees(-inputGyro)
+
+        FLVector = leftStick.avg(FLTurningVector)
+        FRVector = leftStick.avg(FRTurningVector)
+        BLVector = leftStick.avg(BLTurningVector)
+        BRVector = leftStick.avg(BRTurningVector)
+
+        #scalar
+        #find highest value
+        if FLVector.getLength() > FRVector.getLength():
+            maxPowerInput = FLVector.getLength()
+        elif FRVector.getLength() > BLVector.getLength():
+            maxPowerInput = FRVector.getLength()
+        elif BLVector.getLength() > BRVector.getLength():
+            maxPowerInput = BRVector.getLength()
+        else:
+            maxPowerInput = BRVector.getLength()
+        
+        #set scalar value
+        if maxPowerInput > 1:
+            scalar = 1 / maxPowerInput
+        else:
+            scalar = 1
+        
+
+        #completed wheel value assignement
+        FLScaled = FLVector.multiplyBy(scalar)
+        FRScaled = FRVector.multiplyBy(scalar)
+        BLScaled = BLVector.multiplyBy(scalar)
+        BRScaled = BRVector.multiplyBy(scalar)
+
+        
+        data[tags.FLDrive + tags.MOTOR_SPEED_CONTROL] = inputY #wheel output is still always positive for some reason
+        data[tags.FRDrive + tags.MOTOR_SPEED_CONTROL] = inputY
+        data[tags.BLDrive + tags.MOTOR_SPEED_CONTROL] = inputY
+        data[tags.BRDrive + tags.MOTOR_SPEED_CONTROL] = inputY
+
+
+        data[tags.FLSteering] = 0 #will be an angle once pid is created, for now just nothing
+        data[tags.FRSteering] = 0
+        data[tags.BLSteering] = 0
+        data[tags.BRSteering] = 0
 
 
 
--- Robot/utils/V2.py
+++ Robot/utils/V2.py
@@ -1,4 +1,4 @@
-
+import math
 
 
 class V2:
@@ -7,7 +7,58 @@ class V2:
         self.x = x
         self.y = y
 
+    #rotate degrees
+    def rotateDegrees(self, d):
 
+        x = self.x
+        y = self.y
 
+        cos = math.cos(d * (math.pi / 180))
+        sin = math.sin(d * (math.pi / 180))
 
+        return V2(x * cos - y * sin, y * cos + x * sin)
 
+    #length
+    def getLength(self):
+
+        x = self.x
+        y = self.y
+
+        length = math.sqrt((x**2 + y**2))
+
+        return float(length)
+
+    #angle
+    def getAngle(self):
+
+        angle = math.atan2(self.y, self.x)
+
+        return float(angle)
+    
+    #add
+    def add(self, newV2):
+
+        return V2(self.x + newV2.x, self.y + newV2.y)
+    
+    #subtract
+    def subtract(self, newV2):
+
+        return V2(self.x - newV2.x, self.y - newV2.y)
+    
+    #multiply
+    def multiplyBy(self, factor):
+
+        return V2(self.x * factor, self.y * factor)
+    
+    #average
+    def avg(self, newV2):
+
+        averageX = (self.x + newV2.x) / 2
+        averageY  = (self.y + newV2.y) / 2
+
+        return V2(averageX, averageY)
+  
+#Flymer = V2(1.0, 0.6)
+#Flymer = Flymer.rotateDegrees(90)
+
+#print(Flymer)
