80fbb8138cb602f3ba07b58f160294c4c08d0da7
Emmett <unconfigured@null.spigotmc.org>
2023-05-10 19:35:32 -0500

    Comming before something breaks in the worst possible way

--- Robot/swerveController.py
+++ Robot/swerveController.py
@@ -47,23 +47,12 @@ class SwerveProf(Node):
 
         """-----------------------------------------"""
 
-        #raw encoder positions
-        FLPos = data[tags.FLSteering + tags.ENCODER_READING]
-        FRPos = data[tags.FRSteering + tags.ENCODER_READING]
-        BLPos = data[tags.BLSteering + tags.ENCODER_READING]
-        BRPos = data[tags.BRSteering + tags.ENCODER_READING]
-
-
         #Encoder positions as angles and first wrap (0 to 359)
         FLPosAngle = (data[tags.FLSteering + tags.ENCODER_READING] % 1) * 360
         FRPosAngle = (data[tags.FRSteering + tags.ENCODER_READING] % 1) * 360
         BLPosAngle = (data[tags.BLSteering + tags.ENCODER_READING] % 1) * 360
         BRPosAngle = (data[tags.BRSteering + tags.ENCODER_READING] % 1) * 360
 
-        if FLPosAngle < 0:
-            FLPosAngle = FLPosAngle + 360
-
-        """
         if FLPosAngle > 180:
             FLPosAngle = FLPosAngle - 360
         if FRPosAngle > 180:
@@ -71,8 +60,8 @@ class SwerveProf(Node):
         if BLPosAngle > 180:
             BLPosAngle = BLPosAngle - 360
         if BRPosAngle > 180:
-            BRPosAngle = BRPosAngle - 360
-        """
+            BRPosAngle =BRPosAngle - 360
+
         """----------------------------------------------------"""
 
         #set values                  
@@ -81,39 +70,11 @@ class SwerveProf(Node):
         BLTarget = BLVector.getAngle()
         BRTarget = BRVector.getAngle()
 
-        if FLTarget >= 360:
-            FLTarget = FLTarget - 360
-        if FRTarget > 180:
-            FRTarget = FRTarget - 360
-        if BLTarget > 180:
-            BLTarget = BLTarget - 360
-        if BRTarget > 180:
-            BRTarget = BRTarget - 360
-
         FLPower = FLVector.getLength()
         FRPower = FRVector.getLength()
         BLPower = BLVector.getLength()
         BRPower = BRVector.getLength()
 
-        """------------------------------------"""
-        """
-        #find quickest path and change values to it
-        if (FLTarget - FLPosAngle) >= 180:
-            FLTarget = FLTarget + 180
-            FLPower = -FLPower
-
-        if (FRTarget - FRPosAngle) >= 180:
-            FRTarget = FRTarget + 180
-            FRPower = -FRPower
-
-        if (BLTarget - BLPosAngle) >= 180:
-            BLTarget = BLTarget + 180
-            BLPower = -BLPower
-
-        if (BRTarget - BRPosAngle) >= 180:
-            BRTarget = BRTarget + 180
-            BRPower = -BRPower
-        """
         """---------------------------------------"""
 
         #scalar
@@ -147,53 +108,34 @@ class SwerveProf(Node):
         BRPID = PIDController(5.0, 0.0, 0.0)
 
         #calculate error
-
-        #i spent an hour trying to figure out why the error is never set to a negative number EVER
-        #its like 11:30pm now and i still don't know whats wrong
-        #normally i would ask rob but he went to bed after doing school work so im on my own
-        #everything was almost working, and now nothing works - i love it when that happens
-        #i have been working on this since 6pm and it still doesnt work
-        #i hope rob finishes his work soon so that i can get this working
-        #i am making a diary here to distract my mind from the unsolvale problem
-        #it took me like 7 tries to figure out how to spell problem - i should probably go to bed
-        #i just want our stupid serve to work but ive ran through the sim in my head and did all the math this file does
-        #and it got a different result
-        #wtf
-        #goodnight
-        FLSteeringError = (360 - FLPosAngle) - FLTarget
-        FRSteeringError = FRTarget - FRPosAngle
-        BLSteeringError = BLTarget - BLPosAngle
-        BRSteeringError = BRTarget - BRPosAngle
-        
+        FLFakeError = FLTarget - FLPosAngle
+        FLTarget = 0
+        FLPos = -(FLFakeError)
+        FLSteeringError = FLTarget - FLPos
         if FLSteeringError > 180:
-            FLSteeringError = 360 - FLSteeringError
-
+            FLSteeringError = FLSteeringError - 360
+            
+        FRFakeError = FRTarget - FRPosAngle
+        FRTarget = 0
+        FRPos = -(FRFakeError)
+        FRSteeringError = FRTarget - FRPos
         if FRSteeringError > 180:
             FRSteeringError = FRSteeringError - 360
 
+        BLFakeError = BLTarget -BLPosAngle
+        BLTarget = 0
+        BLPos = -(BLFakeError)
+        BLSteeringError = BLTarget - BLPos
         if BLSteeringError > 180:
             BLSteeringError = BLSteeringError - 360
 
+        BRFakeError = BRTarget - BRPosAngle
+        BRTarget = 0
+        BRPos = -(BRFakeError)
+        BRSteeringError = BRTarget - BRPos
         if BRSteeringError > 180:
             BRSteeringError = BRSteeringError - 360
-        """
-        #ignore wrap
-        if (FLSteeringError) >= 180:
-            FLTarget = FLTarget + 180
-            FLPower = -FLPower
-
-        if (FRSteeringError) >= 180:
-            FRTarget = FRTarget + 180
-            FRPower = -FRPower
-
-        if (BLSteeringError) >= 180:
-            BLTarget = BLTarget + 180
-            BLPower = -BLPower
-
-        if (BRSteeringError) >= 180:
-            BRTarget = BRTarget + 180
-            BRPower = -BRPower
-        """
+        
         """============================================="""
 
         #drive values (power)
@@ -205,10 +147,10 @@ class SwerveProf(Node):
         #target steering values (in rotaions)
         data[tags.FLSteering + tags.MOTOR_SPEED_CONTROL] = FLPID.tickErr(FLSteeringError / 360, data[tags.DT])
 
-        data[tags.FRSteering + tags.MOTOR_SPEED_CONTROL] = FRPID.tick(FRSteeringError / 360, FRPosAngle / 360, data[tags.DT])
-        data[tags.BLSteering + tags.MOTOR_SPEED_CONTROL] = BLPID.tick(BLSteeringError / 360, BLPosAngle / 360, data[tags.DT])
-        data[tags.BRSteering + tags.MOTOR_SPEED_CONTROL] = BRPID.tick(BRSteeringError / 360, BRPosAngle / 360, data[tags.DT])
+        data[tags.FRSteering + tags.MOTOR_SPEED_CONTROL] = FRPID.tickErr(FRSteeringError / 360, data[tags.DT])
+        data[tags.BLSteering + tags.MOTOR_SPEED_CONTROL] = BLPID.tickErr(BLSteeringError / 360, data[tags.DT])
+        data[tags.BRSteering + tags.MOTOR_SPEED_CONTROL] = BRPID.tickErr(BRSteeringError / 360, data[tags.DT])
 
-        data[tags.BLSteering + tags.MOTOR_SPEED_CONTROL] = FLTarget #test values
-        data[tags.BRSteering + tags.MOTOR_SPEED_CONTROL] = FLPosAngle
-        data[tags.FRSteering + tags.MOTOR_SPEED_CONTROL] = FLSteeringError
+        #data[tags.BLSteering + tags.MOTOR_SPEED_CONTROL] = FLTarget #test values
+        #data[tags.BRSteering + tags.MOTOR_SPEED_CONTROL] = FLPosAngle
+        #data[tags.FRSteering + tags.MOTOR_SPEED_CONTROL] = FLSteeringError
