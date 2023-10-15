cff7d8996f5c994d7cc206787def49573d9f60ab
robbie aldrich <robbiemaldrich@gmail.com>
2023-10-15 10:17:49 -0500

    Fixed type problems

--- Imperative/encoderSim.py
+++ Imperative/encoderSim.py
@@ -3,6 +3,7 @@ import wpimath.system.plant as plant
 import wpimath.system as sys
 import random
 import math
+import ctre.sensors
 
 # TODO: get real measurements
 ENCODER_STD_DEV = 0.001
@@ -17,7 +18,7 @@ class EncoderSim:
         # NOTE: state[0] is position (in radians), state[1] is velocity (in rads/sec)
         self.state: list[float] = [ 0, 0 ]
 
-    def update(self, dt: float, motor: rev.CANSparkMax, encoder: rev.RelativeEncoder):
+    def update(self, dt: float, motor: rev.CANSparkMax, encoder: ctre.sensors.CANCoder | rev.RelativeEncoder):
         inputVec = [motor.get() * self.motorSpec.nominalVoltage]
         self.state = self.linearSys.calculateX(self.state, inputVec, dt)
         encoder.setPosition(random.normalvariate(self.state[0] / (2 * math.pi), ENCODER_STD_DEV))
