e37df4eb8758e2e62407180c132f9e232648b32a
jackn28 <115190376+jackn28@users.noreply.github.com>
2023-12-07 16:51:59 -0600

    started work on the test bot and shooter thing

---
+++ Imperative/testbox.py
@@ -0,0 +1,16 @@
+import wpilib
+import rev
+
+
+class TestBox(wpilib.TimedRobot):
+    def robotInit(self) -> None:
+        BRUSHLESS = rev.CANSparkMax.MotorType.kBrushless
+        BRUSHED = rev.CANSparkMax.MotorType.kBrushed
+        self.shooterLeft = rev.CANSparkMax(1,BRUSHED)
+        self.shooterRight = rev.CANSparkMax(2,BRUSHED)
+    def robotPeriodic(self) -> None:
+        return super().robotPeriodic()
+    def teleopPeriodic(self) -> None:
+        return super().teleopPeriodic()
+    def disabledPeriodic(self) -> None:
+        return super().disabledPeriodic()
