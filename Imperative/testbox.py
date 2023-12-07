e44e1ba7cb1ccbdfeaefd52cf77300f6a7e8f343
jackn28 <115190376+jackn28@users.noreply.github.com>
2023-12-07 17:00:00 -0600

    added like 4 lines of code for testbot

--- Imperative/testbox.py
+++ Imperative/testbox.py
@@ -4,10 +4,12 @@ import rev
 
 class TestBox(wpilib.TimedRobot):
     def robotInit(self) -> None:
+        #motors======================================================
         BRUSHLESS = rev.CANSparkMax.MotorType.kBrushless
         BRUSHED = rev.CANSparkMax.MotorType.kBrushed
         self.shooterLeft = rev.CANSparkMax(1,BRUSHED)
         self.shooterRight = rev.CANSparkMax(2,BRUSHED)
+        #controllers=================================================
     def robotPeriodic(self) -> None:
         return super().robotPeriodic()
     def teleopPeriodic(self) -> None:
