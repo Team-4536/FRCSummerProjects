3cda775e03e9dc9d93c8f38f7f9fb4862a9ca2f6
jackn28 <115190376+jackn28@users.noreply.github.com>
2023-12-12 16:55:57 -0600

    added all the motors and added motor switching but not all motors are connected to anything

--- Imperative/testbox.py
+++ Imperative/testbox.py
@@ -1,6 +1,14 @@
 import wpilib
 import rev
 
+class TestInputs():
+    def __init__(self, mainCtrlr: wpilib.XboxController) -> None:
+        self.motorSwitch = mainCtrlr.getYButtonPressed()
+        self.leftMainX = mainCtrlr.getLeftX()
+        self.leftMainY = mainCtrlr.getLeftY()
+
+        pass
+
 
 class TestBox(wpilib.TimedRobot):
     def robotInit(self) -> None:
@@ -9,10 +17,55 @@ class TestBox(wpilib.TimedRobot):
         BRUSHED = rev.CANSparkMax.MotorType.kBrushed
         self.shooterLeft = rev.CANSparkMax(1,BRUSHED)
         self.shooterRight = rev.CANSparkMax(2,BRUSHED)
+        self.motor1 = rev.CANSparkMax(3,BRUSHED)
+        self.motor2 = rev.CANSparkMax(4,BRUSHED)
+        self.motor3 = rev.CANSparkMax(5,BRUSHED)
+        self.motor4 = rev.CANSparkMax(6,BRUSHED)
+        self.motor5 = rev.CANSparkMax(7,BRUSHED)
+        self.motor6 = rev.CANSparkMax(8,BRUSHED)
         #controllers=================================================
+        self.mainCtrlr = wpilib.XboxController(0)
+
+        #variables===================================================
+        self.motorAccessed = 1
+
+
+
+
+
+
+
     def robotPeriodic(self) -> None:
+        self.input = TestInputs(self.mainCtrlr)
+        
         return super().robotPeriodic()
+    
+    
     def teleopPeriodic(self) -> None:
+        #switching which motor is accessed
+        if(self.input.motorSwitch == True):
+            if(self.motorAccessed<7)
+                self.motorAccessed = self.motorAccessed + 1
+            else:self.motorAccessed = 1
+        #controlling the motor
+        if(self.motorAccessed == 1):
+            self.shooterLeft.set(self.input.leftMainY * 0.01)
+            self.shooterRight.set(self.input.leftMainY * -0.01)
+        elif(self.motorAccessed == 2):
+            self.motor1.set(self.input.)
+        elif(self.motorAccessed == 3):
+            self.motor2.set(self.input.)
+        elif(self.motorAccessed == 4):
+            self.motor3.set(self.input.)
+        elif(self.motorAccessed == 5):
+            self.motor4.set(self.input.)
+        elif(self.motorAccessed == 6):
+            self.motor5.set(self.input.)
+        elif(self.motorAccessed == 7):
+            self.motor6.set(self.input.)
+
         return super().teleopPeriodic()
+    
+    
     def disabledPeriodic(self) -> None:
         return super().disabledPeriodic()
